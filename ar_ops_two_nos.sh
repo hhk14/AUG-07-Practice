006fb2f6b1b4aef2a8830155316745a6c7bdcfb8
Harisha H K <harishahk875@gmail.com>
2022-08-07 13:29:44 +0530

    modified ar_ops_two_nos.sh to add product of two numbers

--- ar_ops_two_nos.sh
+++ ar_ops_two_nos.sh
@@ -6,4 +6,7 @@ echo "Enter the second number"
 read num2
 sum=0
 sum=$(($num1 + $num2))
+prdt=1
+prdt=$(($num1 * $num2))
 echo "Sum of $num1 and $num2 is : $sum"
+echo "Product of $num1 and $num2 is : $prdt"
